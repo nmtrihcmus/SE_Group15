7cc1c9dc74c598318a29dd8573311f08c49bc686
20120628 <=>
2022-12-31 22:02:18 +0700

    test function list, load list movie using AJAX

--- src/server/configs/cnStr.js
+++ src/server/configs/cnStr.js
@@ -4,7 +4,7 @@ module.exports = {
     database: 'SE_Group15',
     //database: 'db_webxemphim_v1',
     user: 'postgres',
-    //password: '123',
-    password: '070602',
+    password: '123',
+    //password: '070602',
     max: 30 // use up to 30 connections
 };
