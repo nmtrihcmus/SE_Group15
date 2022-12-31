b8a8cc628e2ea55c8b0038282473839539e7d51a
20120628 <=>
2023-01-01 00:44:53 +0700

    update in homepage Top Rating and Phimmoi

--- src/server/controllers/home.c.js
+++ src/server/controllers/home.c.js
@@ -1,15 +1,33 @@
+const movieM = require('../models/movies.m');
 class homeC {
     async homePage(req, res, next) {
         try {
+            const listAll = await movieM.all();
+            var topRating = listAll.sort((a, b)=>{return b.rating-a.rating});
+            
+            var firstRow = topRating.slice(0,3);
+            var secondRow = topRating.slice(3,6);
+            var newMovie = listAll.sort((a, b)=>{return b.insertDate.getTime()-a.insertDate.getTime()});
+           
+            var firstNew = newMovie.slice(0,3);
+            var secondNew = newMovie.slice(3,6);
             if (req.session.username) {
                 return res.render('home', {
                     title: "Home",
+                    firstRow: firstRow,
+                    secondRow: secondRow,
+                    firstNew: firstNew,
+                    secondNew: secondNew,
                     loggedIn: true,
                     isAdmin: req.session.isAdmin
                 });
             }
             return res.render('home', {
                 title: "Home",
+                firstRow: firstRow,
+                secondRow: secondRow,
+                firstNew: firstNew,
+                secondNew: secondNew,
                 loggedIn: false,
             });
         }
